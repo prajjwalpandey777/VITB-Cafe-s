4eb92da71e4a8f68fd6461a9d552a669c2311aab
Prajjwal Pandey <prajjwalpandey171@gmail.com>
2026-05-08 02:47:40 +0530

    Update feedback.js

--- api/routes/feedback.js
+++ api/routes/feedback.js
@@ -15,10 +15,15 @@ const feedbackLimiter = rateLimit({
 
 // Create Gmail transporter
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
